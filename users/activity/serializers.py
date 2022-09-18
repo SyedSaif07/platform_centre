d1215230ba1ea087cc07e3c643f262475d9bcbb1
SyedSaif07 <50112635+SyedSaif07@users.noreply.github.com>
2022-09-18 06:13:29 -0400

    Update README.md

--- users/activity/serializers.py
+++ users/activity/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from activity.models import Activity
+from django.contrib.auth.models import User
 
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = ['id', 'name', 'description', 'created_at', 'updated_at']
+
+class UserSerializer(serializers.ModelSerializer):
+    activity = serializers.PrimaryKeyRelatedField(many=True, queryset=Activity.objects.all())
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'activity']
